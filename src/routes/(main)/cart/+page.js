a4b462ddcb3e6723773aab5579fe3dbd4323f544
damien <yithanglee@gmail.com>
2025-12-01 10:40:39 +0800

    remove sha

--- src/routes/(main)/cart/+page.js
+++ src/routes/(main)/cart/+page.js
@@ -5,20 +5,6 @@ export async function load(event) {
     const { parent } = event;
     const layoutData = await parent();
 
-    console.log('layoutData', layoutData);
-    // layoutData {
-    //     app_routes: [],
-    //     user: {
-    //       country_id: 1,
-    //       username: 'SUZIE',
-    //       token: 'SFMyNTY.g2gDbQAAAAVTVVpJRW4GALlDo72aAWIAAVGA.UsnTSc70NAHEz95-BHFkpuG3y7rUG4tVKV_V8_a453M',
-    //       role_app_routes: [],
-    //       id: 44,
-    //       merchant_id: null
-    //     },
-    //     needLogin: false
-    //   }
-      
     const url = PHX_HTTP_PROTOCOL + PHX_ENDPOINT;
     console.log('url', url);
     const user = layoutData?.user || {};
